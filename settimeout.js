2527a5d893516df63c4506f559fa4bb2c6681d07
DipolarImpala91 <1584089157@qq.com>
2024-11-16 23:39:30 +0800

    Add files via upload
    first submit

---
+++ settimeout.js
@@ -0,0 +1,29 @@
+
+var currentDate = new Date()
+var targetDate = new Date("2025/6/29") // write your target time here
+function diffTime (currentDate,targetDate){
+    var sub = Math.ceil( (targetDate - currentDate ) / 1000 )
+    var day = parseInt( sub/(60*60*24))
+    var hours = parseInt(sub%(60*60*24) / (60*60))
+    var minutes = parseInt(sub%(60*60)/60)
+    var seconds = sub % 60
+    var obj ={
+        day,
+        hours,
+        minutes,
+        seconds
+    }
+    return obj
+}
+
+setInterval(() => {
+    var currentDate = new Date()
+    var res = diffTime(currentDate,targetDate)
+    //#listtime is page name(class or id)
+    document.querySelector("#leasttime").innerHTML = `时间剩余${res.day}天${res.hours}时${res.minutes}分${res.seconds}秒`
+}, 1000);
+// setTimeout(() => {
+//     console.log(currentDate,targetDate)
+// }, 1000);
+
+export default diffTime

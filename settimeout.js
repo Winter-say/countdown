9be0f721becc92cb4290f699af1074319eced834
DipolarImpala91 <1584089157@qq.com>
2024-11-17 15:30:12 +0800

    Update settimeout.js
    Change 'var' to 'let' to prevent naming conflicts

--- settimeout.js
+++ settimeout.js
@@ -1,13 +1,13 @@
 
-var currentDate = new Date()
-var targetDate = new Date("2025/6/29") // write your target time here
+let currentDate = new Date()
+let targetDate = new Date("2025/6/29") // write your target time here
 function diffTime (currentDate,targetDate){
-    var sub = Math.ceil( (targetDate - currentDate ) / 1000 )
-    var day = parseInt( sub/(60*60*24))
-    var hours = parseInt(sub%(60*60*24) / (60*60))
-    var minutes = parseInt(sub%(60*60)/60)
-    var seconds = sub % 60
-    var obj ={
+    let sub = Math.ceil( (targetDate - currentDate ) / 1000 )
+    let day = parseInt( sub/(60*60*24))
+    let hours = parseInt(sub%(60*60*24) / (60*60))
+    let minutes = parseInt(sub%(60*60)/60)
+    let seconds = sub % 60
+    let obj ={
         day,
         hours,
         minutes,
@@ -17,8 +17,8 @@ function diffTime (currentDate,targetDate){
 }
 
 setInterval(() => {
-    var currentDate = new Date()
-    var res = diffTime(currentDate,targetDate)
+    let currentDate = new Date()
+    let res = diffTime(currentDate,targetDate)
     //#listtime is page name(class or id)
     document.querySelector("#leasttime").innerHTML = `时间剩余(Time remaining)${res.day}天(day)${res.hours}时(hours)${res.minutes}分(minutes)${res.seconds}秒(seconds)`
 }, 1000);
